660b6f211ee7bec9e16650557f774043c18478dc
vaughang91 <vaughan.gittel@gmail.com>
2022-02-23 07:02:37 +1000

    +

--- LP_07_classes_objects_player.cpp
+++ LP_07_classes_objects_player.cpp
@@ -14,7 +14,7 @@ inline void keep_window_open() { char ch; std::cin >> ch; }
         
     //methods (fucntions in the class)
         
-        void talk(std::string text_to_say)
+        void talk(std::string text_to_say) //class function or method to get a member and say a string
             {
                 std::cout << name << "says " << text_to_say << std::endl;
             }
@@ -22,6 +22,7 @@ inline void keep_window_open() { char ch; std::cin >> ch; }
         bool is_dead();   
 
     };
+    
     
 int main() {
     //
@@ -32,15 +33,23 @@ int main() {
     vaughan.name = "Vaughan Gittel";
     std::cout << "" << vaughan.name << "\t" << std::endl;
     vaughan.talk("hey fuck face");
-
+    
 
     Player hero; 
-       // hero.name = "Hero"; 
+    hero.name = "Hero"; 
+
+
 
 //create an object on the heap with a pointer address. 
     Player *enemy {nullptr};
-    enemy = new Player; 
+    enemy = new Player;  // create a new object on the heap 
+    enemy->name = "enemy";
+    enemy->talk("i will destroy you kunt");
     delete enemy; 
+
+
+
+
 
 //make a array (or type 'CLASS') of players 
     Player players[]{vaughan, hero};
--- LP_08_classes_objects_bank_account.cpp
+++ LP_08_classes_objects_bank_account.cpp
@@ -3,16 +3,39 @@
 #include<vector>
 #include<algorithm>
 #include<cmath>
+
+#include "LP_09_account_class.h"  //use "" tell the comiler to use header files local to this project. 
+
 inline void keep_window_open() { char ch; std::cin >> ch; }
 
     class Bank_Account{
+    private: 
        //attributes
        std::string name;
-       double ballance = 0;
+       double balance;
 
-       //methods (fucntions in the class)
-       bool withdraw(double amount); //a function that takes in a withdraw ammount and returns a bool 1 if successfull 
-       bool deposit(double amount);             
+    public: 
+       // you need public methods to access private members in the class object   
+       void set_balance(double bal){
+           balance = bal; 
+       }
+       void set_account_name (std::string name){
+           name = name; 
+       } 
+       double get_balance (){
+           return balance;  
+       }             
+       bool withdraw(double bal){
+           balance += bal; 
+           std::cout << "in deposit" << std::endl;
+           return 1; 
+       }; //a function that takes in a withdraw ammount and returns a bool 1 if successfull 
+       
+       bool deposit(double bal){
+            balance -= bal; 
+            std::cout << "in withdraw" << std::endl;  
+            return 1;  
+       };             
     };
     
 int main() {
@@ -21,18 +44,17 @@ int main() {
 
 
     Bank_Account vaughan_gittel_account;
+    vaughan_gittel_account.set_account_name("Vaughans Account");
+    vaughan_gittel_account.set_balance (5000.00); 
+
     Bank_Account kaylee_eljed_account;
 
 
-    int my_num = 69;
-    std::cout << "value of my_num: " << my_num << std::endl;
-    std::cout << "sizeof bytes my_num: " << sizeof my_num << std::endl;
-    std::cout << "memory HEX address of my_num: " << &my_num << std::endl << std::endl;
+    vaughan_gittel_account.deposit(1000.00);
+    vaughan_gittel_account.withdraw(500.00);
 
-    int &my_num_reference = my_num; 
-    std::cout << "my_num_reference: " << my_num_reference << std::endl << std::endl;
-
-    std::vector<std::string> stooges {"larry", "moe", "curly"}; 
+   std::cout << "Vaughans account balance = " << vaughan_gittel_account.get_balance() << std::endl; 
+    
 
 
 
