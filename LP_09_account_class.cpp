2963385288a809bf9366169ba2f46e2f863e26e0
vaughang91 <vaughan.gittel@gmail.com>
2022-02-25 07:44:38 +1000

    update

--- LP_09_account_class.cpp
+++ LP_09_account_class.cpp
@@ -1,12 +1,13 @@
 
 /*
+TO DEFINE A METHOD FUNCTION OUTSIDE OF THE CLASS using "::" 
 this header cpp file holdes the IMPLEMENTATION actual function body and links the function to the class using :: scope operator 
-tells it which class it is linked to...  Bank_Account::my_method()
+tells it which class it is linked to...  Bank_Account::my_method()    
 */
 
 #include"LP_09_account_class.h"
 
-    void Bank_Account::set_balance(double bal){
+    void Bank_Account::set_balance(double bal){    //here we are defining the set_balance method under the Bank_Account Class
            balance = bal; 
        }
 
@@ -22,14 +23,19 @@ tells it which class it is linked to...  Bank_Account::my_method()
            return balance;  
        }             
     
-    bool Bank_Account::withdraw(double bal){
-           balance += bal; 
-           std::cout << "in deposit" << std::endl;
-           return 1; 
+    bool Bank_Account::withdraw(double ammount){
+          if(balance-ammount >= 0){      //verify enough funds availble
+               balance -= ammount; 
+               return true; 
+               std::cout << "Deposit succesful" << std::endl;
+           }else{ 
+                std::cout << "No Funds" << std::endl;
+                return false; 
+           }
        }; //a function that takes in a withdraw ammount and returns a bool 1 if successfull 
-       
-    bool Bank_Account::deposit(double bal){
-            balance -= bal; 
+    
+    bool Bank_Account::deposit(double ammount){
+            balance -= ammount; 
             std::cout << "in withdraw" << std::endl;  
             return 1;  
 
